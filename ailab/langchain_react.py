18d19dc85983838a4863ed4c93a958a12709c696
Xulichenpro <3169463225@qq.com>
2025-10-04 17:11:30 +0800

    design a react agent using langcain

---
+++ ailab/langchain_react.py
@@ -0,0 +1,35 @@
+import os
+import getpass
+from langchain_core.tools import tool
+from langchain_google_genai import ChatGoogleGenerativeAI 
+from langchain.agents import AgentExecutor, create_tool_calling_agent
+from langchain_core.prompts import ChatPromptTemplate
+
+os.environ["GOOGLE_API_KEY"] = getpass.getpass("Please enter gemini api key:")
+print("password entered")
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+@tool
+def magic_function(input:int) -> int:
+    """Applies a magic function to an input."""
+    return input + 2
+
+tools = [magic_function]
+
+query = "what is the value of magic_function(3)?"
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant"),
+        ("human", "{input}"),
+        # Placeholders fill up a **list** of messages
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+)
+
+agent = create_tool_calling_agent(model, tools, prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+agent_executor.invoke({"input": query})
