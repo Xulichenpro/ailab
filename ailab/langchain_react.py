c5feee0efa084754e3298e9f818d31f7127f5644
Xulichenpro <3169463225@qq.com>
2025-10-05 15:05:07 +0800

    a langgraph-based react agent

--- ailab/langchain_react.py
+++ ailab/langchain_react.py
@@ -2,8 +2,9 @@ import os
 import getpass
 from langchain_core.tools import tool
 from langchain_google_genai import ChatGoogleGenerativeAI 
-from langchain.agents import AgentExecutor, create_tool_calling_agent
 from langchain_core.prompts import ChatPromptTemplate
+from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.memory import MemorySaver
 
 os.environ["GOOGLE_API_KEY"] = getpass.getpass("Please enter gemini api key:")
 print("password entered")
@@ -19,30 +20,32 @@ tools = [magic_function]
 
 query = "what is the value of magic_function(3)?"
 
+memory = MemorySaver()
 
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", "You are a helpful assistant"),
-        ("human", "{input}"),
-        # Placeholders fill up a **list** of messages
-        ("placeholder", "{agent_scratchpad}"),
-    ]
+system_message = "You are a helpful assistant."
+
+langgraph_agent_executor = create_react_agent(model, tools, checkpointer=memory,prompt=system_message)
+
+config = {"configurable": {"thread_id": "test-thread","checkpoint_ns":"chat_history"}}
+print(
+    langgraph_agent_executor.invoke(
+        {
+            "messages": [
+                ("user", "Hi, I'm polly! What's the output of magic_function of 3?")
+            ]
+        },
+        config,
+    )["messages"][-1].content
 )
-
-agent = create_tool_calling_agent(model, tools, prompt)
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-
-agent_executor.invoke({"input": query})
-
-from langgraph.prebuilt import create_react_agent
-
-langgraph_agent_executor = create_react_agent(model, tools)
-
-
-messages = langgraph_agent_executor.invoke({"messages": [("human", query)]})
-
-result = {
-            "input": query,
-            "output": messages["messages"][-1].content,
-        }
-print(result)
+print("---")
+print(
+    langgraph_agent_executor.invoke(
+        {"messages": [("user", "Remember my name?")]}, config
+    )["messages"][-1].content
+)
+print("---")
+print(
+    langgraph_agent_executor.invoke(
+        {"messages": [("user", "what was that output again?")]}, config
+    )["messages"][-1].content
+)
